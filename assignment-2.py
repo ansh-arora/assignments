5559f41aa9db2928a19b5a2d2563afdd0bc64c1f
Ansh Arora <ansh.arora3098@gmail.com>
2018-05-30 11:31:54 +0530

    assignment-2

---
+++ assignment-2.py
@@ -0,0 +1,21 @@
+# Q1
+print("hello world")
+# Q2
+print("acad"+"view")
+# Q3
+x=input("enter first number")
+y=input("enter second number")
+z=input("enter third number")
+print(x,y,z)
+# Q4
+print('let\'s get started')
+# Q5
+s="Acadview"
+course="python"
+fee=5000
+print('%s,%s,%d'%(s,course,fee))
+# Q6
+name="Tony Stark"
+salary=1000000
+print('%s"%d'%(name,salary))
+
