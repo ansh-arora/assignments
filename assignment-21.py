c7877856799f67320ef5e7bfe0fb473acb3f35aa
Ansh Arora <ansh.arora3098@gmail.com>
2018-06-23 15:55:48 +0530

    pandas

---
+++ assignment-21.py
@@ -0,0 +1,31 @@
+#Q1
+import pandas as pd
+# data = {'Name':['Anmol'],'Age':[20],'Email ID':['anmol@gmail.com'],'Phone No.':[9988776655]}
+# i = pd.DataFrame(data)
+# i.loc[1]=['Dhruv',20,'dhruv@yahoo.com',9898989898]
+# print(i)
+
+# Q2
+# a.)
+# df=pd.read_csv('data.csv')
+# print(df.head(5))
+# b.)
+# df=pd.read_csv('data.csv')
+# print(df.head(10))
+# c.)
+# df=pd.read_csv('data.csv')
+# d=pd.Series((df.loc[:,'MinTemp']))
+# s=d.min()
+# g=((df.loc[df['MinTemp']==s,'Date']))
+# print('Min Temperature is:',s,'on:',g.values)
+# d.)
+# df=pd.read_csv('data.csv')
+# print(df.tail(5))
+# e.)
+# df=pd.read_csv('data.csv')
+# d=pd.Series((df.loc[:,'MinTemp']))
+# s=d.min()
+# s2=d.max()
+# print('Minimum temperature lies between',s,'to',s2)
+
+
