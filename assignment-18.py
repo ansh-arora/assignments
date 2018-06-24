8fc29ae8dd739384279c1e946f3957b14b7b9105
Ansh Arora <ansh.arora3098@gmail.com>
2018-06-25 02:40:10 +0530

    gui_1

---
+++ assignment-18.py
@@ -0,0 +1,40 @@
+#Q1
+from tkinter import *
+# main= Tk()
+# Label(main, text='Hello World!').pack()
+# Button(main, text='Exit',command= sys.exit, activeforeground='red' ).pack()
+# mainloop()
+
+#Q2
+# def press():
+#     print('hiii')
+#     Label(main, text='Hello Again !!').pack()
+# main= Tk()
+# Label(main, text='Hello World!').pack()
+# Button(main, text='Exit',command= sys.exit, activeforeground='red' ).pack()
+# Button(main, text='Press me !',command= press, activeforeground='Blue' ).pack()
+# mainloop()
+
+#Q3
+# main=Tk()
+# def change():
+#     l.configure(text='Hi hello!!')
+# f=Frame(main).pack()
+# l=Label(f, text='hello')
+# l.pack()
+# Button(f, text='Press me to change the text', activeforeground='blue',command=change).pack()
+# Button(main, text='Exit',command= sys.exit, activeforeground='red' ).pack()
+# mainloop()
+
+#Q4
+# def p(e):
+#     print('your name is:',e)
+#     Label(main, text='You entered your name as :').grid(row=1)
+#     Label(main, text=e).grid(row=1,column=1)
+# main=Tk()
+# Label(main,text='Your name').grid(row=0)
+# e=Entry(main)
+# e.grid(row=0,column=1)
+# Button(main, text='Submit', activeforeground='blue',command=lambda :p(e.get())).grid(row=0,column=2)
+# mainloop()
+
