88e13c34d390c056ab5e554d9413affb94bbcdba
Ansh Arora <ansh.arora3098@gmail.com>
2018-06-26 17:05:28 +0530

    gui_2

--- assignment-20.py
+++ assignment-20.py
@@ -35,22 +35,17 @@ mainloop()
 
 #Q2
 # from tkinter import *
-#
+# #
 # root = Tk()
 # f2=Frame(root)
 # f2.pack()
 # d={'ansh':9898989898,'dhruv':8787878787}
+# set1=set(d)
 # lab=Label(f2)
 # lab.pack(side=BOTTOM)
 # lab1=Label(f2)
 # lab1.pack(side=BOTTOM)
 #
-# def ins():
-#     x=input('enter new name:')
-#     y=input('enter phone number:')
-#     d[x]=y
-#     d1=d
-#     print(d)
 #
 # def selected():
 #
@@ -62,18 +57,52 @@ mainloop()
 #         lab1.config(text='Phone number:')
 #         lab.config(text=d[n])
 #
-# s=list(d)
+#
 #
 # scrollbar = Scrollbar(f2)
 # scrollbar.pack( side = RIGHT, fill = Y )
 # mylist = Listbox(f2, yscrollcommand = scrollbar.set)
-# for names in s :
-#    mylist.insert(END, names)
 # mylist.pack( fill = BOTH )
 # scrollbar.config( command = mylist.yview )
+#
+# def ins():
+#
+#     f=Frame(root)
+#     f.pack()
+#     Label(f,text='Name:').grid(row=0)
+#     n=Entry(f)
+#     n.grid(row=0,column=1)
+#     Label(f, text='Phone:').grid(row=1)
+#     p = Entry(f)
+#     p.grid(row=1, column=1)
+#     Button(f,text='Submit',command=lambda :add1(n.get(),p.get())).grid(row=2,column=1)
+# def add():
+#     s = list(d)
+#     for names in s:
+#         mylist.insert(END, names)
+#
+# def add1(n1,p1):
+#     if n1:
+#         d[n1]=p1
+#         global set1
+#     set2=set(d)
+#     set3=set2-set1
+#     set1=set2
+#     s=list(set3)
+#     for names in s :
+#         mylist.insert(END, names)
+# add()
 # f=Frame(root)
 # f.pack()
 # Button(f, text='Enter New Details', command=ins).pack()
 # selected()
 # mainloop()
+
+
+
+
+
+
+
+
 
